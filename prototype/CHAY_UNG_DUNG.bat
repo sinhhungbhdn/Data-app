821bb5072cb2eed8d016f0de1a3812c4ed7c170d
sinhhungbhdn <sinhhungbhdn@gmail.com>
2026-07-14 17:09:34 +0700

    Bỏ phụ thuộc localhost khi chạy ứng dụng

--- prototype/CHAY_UNG_DUNG.bat
+++ prototype/CHAY_UNG_DUNG.bat
@@ -1,16 +1,5 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-where py >nul 2>nul
-if %errorlevel%==0 (
-  start "" http://localhost:8080
-  py -m http.server 8080
-  exit /b
-)
-where python >nul 2>nul
-if %errorlevel%==0 (
-  start "" http://localhost:8080
-  python -m http.server 8080
-  exit /b
-)
-start "" index.html
+start "" "%~dp0index.html"
+exit /b
